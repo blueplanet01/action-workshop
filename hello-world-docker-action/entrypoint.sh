dea8aa4efcb7bc1ed3af9b97a2781d9e2830df04
blueplanet01 <35351739+blueplanet01@users.noreply.github.com>
2025-03-18 10:46:02 +0000

    2 variables.

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -2,5 +2,5 @@
 
 echo "hello $1"
 
-# echo "time=$(date), user=$1" >> $GITHUB_OUTPUT
-echo "time=$(date)" >> $GITHUB_OUTPUT
+echo "time=$(date) user=$1" >> $GITHUB_OUTPUT
+# echo "time=$(date)" >> $GITHUB_OUTPUT
