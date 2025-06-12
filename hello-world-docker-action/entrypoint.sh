0993177b89c10b134e782de5cbcc7e265ae202b4
Yan-Hong_nbs <yan.hong@nationwide.co.uk>
2025-06-12 15:09:48 +0100

    first docker action

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -2,5 +2,5 @@
 
 echo "hello $1"
 user=$1
-echo "time=$(date) who=$user" >> $GITHUB_OUTPUT
-# echo "time=$(date)" >> $GITHUB_OUTPUT
+# echo "time=$(date) who=$user" >> $GITHUB_OUTPUT
+echo "time=$(date)" >> $GITHUB_OUTPUT
